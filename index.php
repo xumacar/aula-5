b606f03556defe3612b6fc22b9445f122be4dd02
xumacar <mikaelschumi826@gmail.com>
2024-06-10 10:48:27 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+    <body>
+        <form id="formCadastro" name="formCadastro" metrod="POST" action="recebeForm.php">
+            <!-- Aqui são inseridos os demais campos do formulário -->
+            Campo 1: <input type=text name=campo1><br>
+            Campo 2: <input type=text name=campo2><br>
+            <!-- Aqui são inseridos os demais campos do formulário -->
+
+            <input id="botaoEnviar" type="submit" value="Enviar">
+        </form>
+    </body>
+</html>
---
+++ recebeForm.php
@@ -0,0 +1,8 @@
+<html>
+    <body>
+        <?php
+            echo "O valor de Campo 1 é:" . $_POST["campo1"];
+            echo "<br>O valor de Campo 2 é:" . $_POST["CAMPO2"];
+        ?>
+    </body>
+</html>   
